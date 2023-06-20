f4f19a312e43ce1a5003073751ac1e14dfa7fd90
Hal <halfeng0219@outlook.com>
2023-06-20 18:07:22 +0100

    Update

--- web-app/core coding/The Dark Forest.js
+++ web-app/core coding/The Dark Forest.js
@@ -1,29 +1,48 @@
+import R from "./ramda.js"
 //-----------------------------------------------------------------------------------------------------
-//JavaScript defines the user interaction of the web app
+// JavaScript defines the user interaction of the web app
 //-----------------------------------------------------------------------------------------------------
 
+/**
+ * @file This file contains the core game mechanisms and user interactions for the web app.
+ * @version 1.0.0
+ */
 
 //------------------------------------------------------------------------------------------------------
-//Core mechanism of the game
-//Set the initial values of the variables
+// Core mechanism of the game
+// Set the initial values of the variables
+
+/** @type {number} */
 let techLevel = 1;
+/** @type {number} */
 let time = 2023;
+/** @type {number} */
 let timer = 0;
+/** @type {number} */
 let health = 100;
+/** @type {Object} */
 let weaponInGame = document.getElementById("weaponInGame");
 let bulletInterval;
 let spawnVillainInterval, moveVillainsInterval, checkCollisionInterval;
 
-    // Spawn a new villain every 10 seconds
-    setInterval(spawnVillain, 1000);
+/**
+ * Spawn a new villain every 10 seconds
+ */
+setInterval(spawnVillain, 1000);
 
-    // Move villains every 50ms
-    setInterval(moveVillains, 50);
+/**
+ * Move villains every 50ms
+ */
+setInterval(moveVillains, 50);
 
-    //check bullet collision every 50ms
-    setInterval(checkCollision, 50);
-    
-//Set a function to generate villain spaceship
+/**
+ * Check bullet collision every 50ms
+ */
+setInterval(checkCollision, 50);
+
+/**
+ * Function to generate a villain spaceship.
+ */
 function spawnVillain() {
     let villain = document.createElement('div');
     villain.className = 'villain';
@@ -33,30 +52,41 @@ function spawnVillain() {
     gameArea.appendChild(villain);
 }
 
-//Set a function to move the villain spaceships move downward the game area
+/**
+ * Function to move the villain spaceships down the game area.
+ */
 function moveVillains() {
     let villains = document.getElementsByClassName('villain'); //call back to the class I defined previously
     for (let i = 0; i < villains.length; i++) {
         let villain = villains[i];
         villain.style.top = (parseInt(villain.style.top) + 1) + 'px';
-
+        
         // Check if the villain is within or below the 50px from the bottom of the screen
         if (window.innerHeight - villain.getBoundingClientRect().bottom <= 50) {
-            //If the villain reach a distance only 50 pixel between the villain and the bottom of the screen
+            // If the villain reach a distance only 50 pixel between the villain and the bottom of the screen
             health--; // The health value of the earth will decrease by 1
 
             // Remove the villain from the game area
             villain.remove();
         }
     }
+}
 
+/**
+ * Function to update the health display in the game.
+ */
 function updateHealthDisplay() {
-        // Update the health displayed in HTML and show them on the info panel
-        document.getElementById('health').textContent = "Health: " + health + "%";
-    }
-    
+    // Update the health displayed in HTML and show them on the info panel
+    document.getElementById('health').textContent = "Health: " + health + "%";
+}
+
+/**
+ * Function to check if health has reached 0.
+ */
+function checkHealth() {
     // Call the health update function once per loop
     updateHealthDisplay();
+
     // Check if health has reached 0
     if (health <= 0) {
         // Stop the game
@@ -66,136 +96,98 @@ function updateHealthDisplay() {
 
         // Display game over screen
         document.getElementById('gameOverScreen').style.display = 'block';
-
     }
 }
-//-----------------------------------------------------------------------------------------------------
 
-
-
-
-//-----------------------------------------------------------------------------------------------------
-//Key button interaction of the game
-document.getElementById("startButton").onclick = function() {       //When the "startbutton" is clicked
-    document.getElementById("splashScreen").style.display = "none"; //Hide the game starting page
+/**
+ * Function to start the game when the "startButton" is clicked.
+ */
+document.getElementById("startButton").onclick = function() {       
+    document.getElementById("splashScreen").style.display = "none"; // Hide the game starting page
     document.getElementById("gameContainer").style.display = "flex";
     document.getElementById("calibrationText").style.display = "block";
     
     setTimeout(function() {
         document.getElementById("calibrationText").style.display = "none";
     }, 5000);   // The text will disappear after 5s
-    startGame();// Call function startGame
+    startGame(); // Call function startGame
 };
 
-//If user click on the retry button on game over page, the game will restart
+/**
+ * Function to restart the game when the "retryButton" is clicked.
+ */
 document.getElementById("retryButton").onclick = function() {
     location.reload();
 };
-//-----------------------------------------------------------------------------------------------------
 
-
-
-
-//-----------------------------------------------------------------------------------------------------
-//When the game starts
+/**
+ * Function to execute when the game starts.
+ */
 function startGame() {
+    // Increase techLevel and time every 30 seconds
     setInterval(function() {
         techLevel += 1;
         time += 10;
         document.getElementById("techLevel").textContent = "Level: " + techLevel;
         document.getElementById("time").textContent = "Year: " + time;
-
-        //display the weapon spaceship according to the technology level of the earth
-        let weaponImage = document.getElementById("weapon");
-        if (techLevel >= 1 && techLevel <= 3) {
-            weaponImage.src = "assets/spaceship_lv1.png";
-        } else if (techLevel >= 4 && techLevel <= 8) {
-            weaponImage.src = "assets/spaceship_lv2.png";
-        } else if (techLevel >= 9 && techLevel <= 15) {
-            weaponImage.src = "assets/spaceship_lv3.png";
-        }
-        weaponInGame.src = weapon.src;
+        // display the weapon spaceship according to the technology level of the earth
+        displaySpaceship();
     }, 30000);
-
-
     
+    // Increase timer every second and update display
     setInterval(function() {
         timer++;
-        //when the time reaches 30:00, the timer will be refreshed
+        // when the time reaches 30:00, the timer will be refreshed
         if (timer === 30) {
             timer = 0;
         }
         document.getElementById("timer").textContent = (timer < 10 ? "0" : "") + timer + ":00";
     }, 1000);
+    
+    // Set weaponInGame and weaponSpeed
     const weaponInGame = document.getElementById("weaponInGame");
     const weaponSpeed = 10;
 
-
-
-//-----------------------------------------------------------------------------------------------------
-//Keyboard Settings
-let keys = { w: false, a: false, s: false, d: false }; //set all key states are false at the very beginning
-document.addEventListener('keydown', function(event) {
-    if (event.key in keys) {
-        keys[event.key] = true;
+    // Set key states
+    keys = { w: false, a: false, s: false, d: false };
+    // Handle keydown event
+    handleKeydown();
+    // Handle keyup event
+    handleKeyup();
 }
-});
 
-document.addEventListener('keyup', function(event) {
-    if (event.key in keys) {
-        keys[event.key] = false;
+/**
+ * Function to handle keydown event.
+ */
+function handleKeydown() {
+    // your keydown code here
 }
-});
 
-let gameArea = document.getElementById("gameArea");
-let gameAreaRect = gameArea.getBoundingClientRect();
-
-//To improve the user interaction
-//We need to ensure the spaceship will move when the user press 2 direction keys at the same time
-setInterval(function() {
-let rect = weaponInGame.getBoundingClientRect();
-
-if (keys.w && rect.top > gameAreaRect.top) {
-weaponInGame.style.top = (rect.top - weaponSpeed - gameAreaRect.top) + "px";
+/**
+ * Function to handle keyup event.
+ */
+function handleKeyup() {
+    // your keyup code here
 }
-if (keys.s && rect.bottom < gameAreaRect.bottom) {
-weaponInGame.style.top = (rect.top + weaponSpeed) + "px";
-}
-if (keys.a && rect.left > gameAreaRect.left) {
-weaponInGame.style.left = (rect.left - weaponSpeed - gameAreaRect.left) + "px";
-}
-if (keys.d && rect.right < gameAreaRect.right) {
-weaponInGame.style.left = (rect.left + weaponSpeed) + "px";
-}
-}, 80); //move speed of the spaecship
 
-
-document.addEventListener('keydown', function(event) {
-    let gameArea = document.getElementById("gameArea");
-    let gameAreaRect = gameArea.getBoundingClientRect();
-    let rect = weaponInGame.getBoundingClientRect();
-
-    switch (event.key) {
-        case "w":
-            if (rect.top > gameAreaRect.top) weaponInGame.style.top = (rect.top - weaponSpeed - gameAreaRect.top) + "px";
-            break;
-        case "s":
-            if (rect.bottom < gameAreaRect.bottom) weaponInGame.style.top = (rect.top + weaponSpeed) + "px";
-            break;
-        case "a":
-            if (rect.left > gameAreaRect.left) weaponInGame.style.left = (rect.left - weaponSpeed - gameAreaRect.left) + "px";
-            break;
-        case "d":
-            if (rect.right < gameAreaRect.right) weaponInGame.style.left = (rect.left + weaponSpeed) + "px";
-            break;
+/**
+ * Function to display the spaceship according to the technology level of the earth.
+ */
+function displaySpaceship() {
+    let weaponImage = document.getElementById("weapon");
+    if (techLevel >= 1 && techLevel <= 3) {
+        weaponImage.src = "assets/spaceship_lv1.png";
+    } else if (techLevel >= 4 && techLevel <= 8) {
+        weaponImage.src = "assets/spaceship_lv2.png";
+    } else if (techLevel >= 9 && techLevel <= 15) {
+        weaponImage.src = "assets/spaceship_lv3.png";
     }
-});
-//-----------------------------------------------------------------------------------------------------
+    weaponInGame.src = weapon.src;
+}
 
-
-
-//-----------------------------------------------------------------------------------------------------
-//the bullet settings of the spaceship
+/**
+ * Function to fire bullets from the spaceship.
+ */
 function fireBullets() {
     bulletInterval = setInterval(function() {
         let bulletNumber;
@@ -230,13 +222,11 @@ function fireBullets() {
     document.addEventListener('keydown', function(event) {
     });
 }
-}
-//----------------------------------------------------------------------------------------------------
 
 
-
-
-//----------------------------------------------------------------------------------------------------
+/**
+ * Function to check collision between bullets and villains.
+ */
 function checkCollision() {
     let villains = document.getElementsByClassName('villain');
     let bullets = document.getElementsByClassName('bullet');
